acd59b473de7b62c23496f7ab012c623b1863d26
nayyarv <nayyarv@gmail.com>
2018-10-06 23:48:22 +1000

    first commit

---
+++ bandit/bandit.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+import numpy as np
+import gym
+import click
+
+import gym_bandits
+
+ZSCORE = 1.5
+
+class Qtable:
+    """
+    Base class to record and update Q.
+    Note I use nums=0 since I've used a slightly different formulation
+    """
+    def __init__(self, num=10):
+        self.num_table=np.zeros(num)
+        self.q = np.zeros(num)
+
+    def update(self, action, reward):
+        raise NotImplementedError
+    
+    def exploit(self):
+        return np.argmax(self.q)
+
+    def score(self):
+        return np.dot(self.num_table, self.mu)
+
+class QAverage(Qtable):
+    def update(self, action, reward):
+        self.q[action] = (self.q[action] * self.num_table[action] + reward) /(self.num_table[action]+1)
+        self.num_table[action]+=1
+        self.mu = self.q
+    
+class QUCB(Qtable):
+    """
+    Uses q = mu + Z * se(mu)
+    In this case se(mu) is just sqrt(sample variance). 
+    Z is 1.96 for alpha=5%, but you can adapt to affect sureness. The greater Z the smaller the
+    deviance from mu and thus the higher exploitation rate. Z = 0 is the same as QAverage
+    """
+    def __init__(self, num=10):
+        self.mu = np.zeros(num)
+        self.ss = np.zeros(num)
+        super().__init__(num)
+
+    def update(self, action, reward):
+        self.mu[action] = (self.mu[action] * self.num_table[action] + reward) /(self.num_table[action]+1)
+        self.ss[action] = (self.ss[action] * self.num_table[action] + reward**2)/(self.num_table[action]+1) 
+        self.num_table[action]+=1
+        self.q[action] = self.mu[action] + ZSCORE * (np.sqrt(self.ss[action] - self.mu[action]**2))
+
+# eps functions
+
+def exponential(value, cst=20):
+    return 1 - np.exp(-cst*value)
+
+def invsq(value, cst=10):
+    return 1 - 1/(1+cst*x**2)
+
+def linear(value, _):
+    return value
+
+def const(value, cst=0.9):
+    return cst
+
+epsDict = {
+    "exponential": exponential,
+    "invsq": invsq,
+    "linear": linear,
+    "const": const
+}
+
+
+def percDecision(value):
+    """exploitation vs exploration."""
+    return np.random.random() < value
+
+
+@click.command()
+@click.option("--method", type=click.Choice(["Ave", "UCB"]), default="Ave")
+@click.option("--bandit-seed", type=int, help="set bandit values seed")
+@click.option("--seed", type=int, help="set exploration seed. Unset leaves it independent of banditseed")
+@click.option("--numbandits", default=10)
+@click.option("-N", default=1000)
+@click.option("--eps-func", default="const", type=click.Choice(epsDict.keys())) # overrides epsilon
+@click.option("--eps-hp", type=float, help="If eps-func takes a parameter, this sets it. Look at code") 
+def main(method, bandit_seed, seed, numbandits, n, eps_func, eps_hp):
+    N = n
+    if bandit_seed:
+        np.random.seed(bandit_seed)
+
+    def eps(x):
+        if eps_hp:
+            # maybe a partial?
+            f = lambda y: epsDict[eps_func](y, eps_hp)
+        else:
+            f =  epsDict[eps_func]
+        return f(x)
+
+    env = gym.make('BanditTenArmedGaussian-v0')
+    obs = env.reset()
+
+    if seed:
+        np.random.seed(seed)
+    else:
+        # reset randomness
+        np.random.seed()
+
+    explCount = 0
+    if method == "Ave":
+        q_table = QAverage(numbandits)
+    else:
+        q_table = QUCB(numbandits)
+
+    for i in range(N):
+        # continue
+        if percDecision(eps(i/N)):
+            action = q_table.exploit()
+            explCount += 1
+        else:
+            # explore time
+            action = np.random.randint(numbandits)
+
+        _, reward, *_ = env.step(action)
+
+        q_table.update(action, reward)
+
+
+
+    actuals = [mu for mu, _ in env.env.r_dist]
+    truebest = np.argmax(actuals)
+    bestPerf = np.argmax(q_table.q)
+
+    np.set_printoptions(precision=2)
+    print(f"N = {N}, Qfunc: {method}, seed = {seed}")
+    print("Num", q_table.num_table)
+    print("Q  ", q_table.q)
+    print("Mu ", q_table.mu)
+    print(f"MAB  Id: {bestPerf}, Ave: {q_table.mu[bestPerf]:.2f}, ntimes: {q_table.num_table[bestPerf]}")
+    print(f"True Id: {truebest}, Ave: {actuals[truebest]:.2f}")
+
+    score = q_table.score() / (N * actuals[truebest])
+    print(f"Score: {score:.2f}")
+    print(f"nexploit: {explCount}, nexplore: {N-explCount}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ policy/policyvalue.py
@@ -0,0 +1,124 @@
+#  -*- coding: utf-8 -*-
+__author__ = "Varun Nayyar <nayyarv@gmail.com>"
+
+import numpy as np
+from rlenvs.envs.gridworld import GridworldEnv
+
+
+def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
+    """
+    Evaluate a policy given an environment and a full description of the environment's dynamics.
+
+    Args:
+        policy: [S, A] shaped matrix representing the policy.
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment.
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+
+    Returns:
+        Vector of length env.nS representing the value function.
+    """
+    # Start with a random value function. Let's set endpoints to 0
+    V = np.random.random(env.nS) * env.nS
+    V[0] = 0
+    V[-1] = 0
+
+    deltaV = np.ones(env.nS)
+    while np.sum(deltaV ** 2) > theta:
+        for state in range(env.nS):
+            v = 0
+            for action, action_transitions in env.P[state].items():
+                ps = 0
+                # for each action taken, we have a bunch of possible state transitions outcomes
+                # in this case we have a perfect model, so we have only 1 transition, but for posterity
+                for transition, nextstate, reward, done in action_transitions:
+                    ps += transition * (reward + discount_factor * V[nextstate])
+                v += policy[state][action] * ps
+
+            deltaV[state] = V[state] - v
+            V[state] = v
+        print(V, deltaV)
+    return V
+
+
+def value_iteration(env, theta=0.0001, discount_factor=1.0):
+    """
+    Value Iteration Algorithm.
+
+    Args:
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment.
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+
+    Returns:
+        A tuple (policy, V) of the optimal policy and the optimal value function.
+    """
+
+    V = np.zeros(env.nS)
+
+    # Task 3 (or 2.5) - Ignore policy for task 2
+    policy = np.zeros([env.nS, env.nA])
+
+    # Implement!
+    return policy, V
+
+
+
+def main():
+    # setup
+    env = GridworldEnv()
+
+    # policy evaluation
+    random_policy = np.ones([env.nS, env.nA]) / env.nA
+    v_valuation = policy_eval(random_policy, env)
+    print(v_valuation)
+
+    # policy value iteratio
+
+    policy, v = value_iteration(env)
+
+    print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+    print(np.reshape(np.argmax(policy, axis=1), env.shape))
+    print("")
+
+    print("Value Function:")
+    print(v)
+    print("")
+
+    print("Reshaped Grid Value Function:")
+    print(v.reshape(env.shape))
+    print("")
+
+    # Task 3, check policy
+    print("Policy Probability Distribution:")
+    print(policy)
+    print("")
+
+if __name__ == '__main__':
+    main()
+
+
+import pytest
+
+@pytest.fixture()
+def env():
+    return GridworldEnv()
+
+def test_value(env):
+    random_policy = np.ones([env.nS, env.nA]) / env.nA
+    v = policy_eval(random_policy, env)
+    expected_v = np.array([0, -14, -20, -22, -14, -18, -20, -20, -20, -20, -18, -14, -22, -20, -14, 0])
+    np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
+
+
+def test_pol_iter(env):
+    policy, v = value_iteration(env)
+    # Test the value function
+    expected_v = np.array([0, -1, -2, -3, -1, -2, -3, -2, -2, -3, -2, -1, -3, -2, -1, 0])
+    np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
