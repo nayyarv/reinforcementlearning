557d9ceaae65008331be6031055c84f12cf36301
nayyarv <nayyarv@gmail.com>
2018-10-11 20:43:32 +1100

    numpy q table

--- tabularq/tq.py
+++ tabularq/tq.py
@@ -1,68 +1,67 @@
 #  -*- coding: utf-8 -*-
 __author__ = "Varun Nayyar <nayyarv@gmail.com>"
 
-# from tabularq.frozenlake import *
-
 import random
+import numpy as np
 
-import collections
 
+class QTable:
+    def __init__(self, nS, nA):
+        self.Q = np.zeros((nS, nA))
 
-class QTable():
-    def __init__(self, num_actions=4):
-        self.num_actions = num_actions
-        self.Q = collections.defaultdict(lambda: [0] * num_actions)
+    def __getitem__(self, item):
+        return self.Q[item]
 
-    def get_Q(self, s, a):
+    def __setitem__(self, instance, value):
+        self.Q[instance] = value
+
+    def get_Q(self, state, action):
         """Q(s, a): get the Q value of (s, a) pair"""
-        return self.Q[s][a]
+        return self.Q[state][action]
 
-    def get_max_Q(self, s):
-        """max Q(s): get the max of all Q value of state s"""
-        return max(self.Q[s])
-
-    def set_Q(self, s, a, q):
+    def set_Q(self, state, action, q):
         """Q(s, a) = q: update the q value of (s, a) pair"""
-        self.Q[s][a] = q
+        self.Q[state][action] = q
 
-    def get_max_a_for_Q(self, s):
+    def get_max(self, state):
+        """max Q(s): get the max of all Q value of state s"""
+        return np.max(self.Q[state])
+
+    def get_max_a(self, state):
         """argmax_a Q(s, a): get the action which has the highest Q in state s"""
 
-        mx = self.get_max_Q(s)
-        return random.choice([i for i, j in enumerate(self.Q[s]) if j == mx])
+        mx = self.get_max(state)
+        return random.choice(np.argwhere(self.Q[state] == mx))[0]
 
     def __str__(self):
         output = []
-        for s in self.Q:
-            output.append(s.__str__() + ": " + ["{:07.4f}".format(a) for a in self.Q[s]].__str__())
-        output.sort()
-        return "QTable (number of actions = " + str(self.num_actions) + ", states = " + str(
-            len(output)) + "):\n" + "\n".join(output)
+        nS, nA = self.Q.shape
+        for s, state in enumerate(self.Q):
+            output.append(f"{s:<2}: {state.round(2)}")
+        return f"QTable (number of actions {nA}, states = {nS}):\n" + '\n'.join(output)
 
 
 import pytest
 
-
 @pytest.fixture()
 def Qt():
-    return QTable(num_actions=4)
+    return QTable(nA=4, nS=16)
 
 
 def test_Qtable1(Qt):
-    s = tuple([5, 6])
     a = 1
-    assert Qt.get_Q(s, a) == 0, "Q value should be 0 to start with"
+    assert Qt[(11, a)] == 0, "Q value should be 0 to start with"
 
 
 def test_Qtable2(Qt):
     a = 1
-    s = tuple([5, 3])
+    s = 15
     Qt.set_Q(s, a, 90)
-    assert Qt.get_Q(s, a) == 90, "Updated Q value should equal 90"
+    assert Qt[(s, a)] == 90, "Updated Q value should equal 90"
 
     a = 2
     Qt.set_Q(s, a, 85)
-    assert Qt.get_max_Q(s) == 90, "Max Q should be 90"
-    assert Qt.get_max_a_for_Q(s) == 1, "Max action for state should be 1"
+    assert Qt.get_max(s) == 90, "Max Q should be 90"
+    assert Qt.get_max_a(s) == 1, "Max action for state should be 1"
 
     print(Qt)
